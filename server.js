b42f1703f00bc83d6b5a514d3f38ae375d73c7cb
Basiakus <panadambasiak@gmail.com>
2017-12-26 23:21:52 +0100

    comit

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var app = express();
-var stringifyFile;
-var fs = require('fs');
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const notes = "notes: "
+let stringifyFile = ([notes]);
+const fs = require('fs');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
 app.get('/getNote', function(req, res) {
 	fs.readFile('./testJson.json', 'utf-8', function(err, data) {
 		if(err) throw err;
-		stringifyFile = data
+		stringifyFile = [data]
 		res.send(data);
 	});
 });
-app.post('/getNotes/:note', function(req, res) {
+app.post('/updateNote/:note', function(req, res) {
 	fs.readFile('./testJson.json', 'utf-8', function(err, data) {
 		if(err) throw err;
-		stringifyFile = data + req.params.note
+		stringifyFile = [data, req.params.note]
 		fs.writeFile('./testJson.json', stringifyFile, function(err, data) {
 			if(err) throw err;
 			res.send(stringifyFile);
