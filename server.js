6000667b906be6244394fb6a5005f7135a9f9e4e
Basiakus <panadambasiak@gmail.com>
2017-12-27 17:49:26 +0100

    well done songit add .!

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ const app = express();
 let stringifyFile;
 const fs = require('fs');
 const bodyParser = require('body-parser');
+let fileToObj;
 app.use(bodyParser.json());
 
 app.get('/getNote', function(req, res) {
 	fs.readFile('./testJson.json', 'utf-8', function(err, data) {
 		if(err) throw err;
-		stringifyFile = [data];
+		stringifyFile = data;
 		res.send(data);
 	});
 });
+
+
+
 app.post('/updateNote/:note', function(req, res) {
 	fs.readFile('./testJson.json', 'utf-8', function(err, data) {
 		if(err) throw err;
-		stringifyFile = [data, req.params.note]
+		fileToObj = JSON.parse(data);
+		fileToObj.notes.push(req.params.note);
+		stringifyFile = JSON.stringify(fileToObj);
+
 		fs.writeFile('./testJson.json', stringifyFile, function(err, data) {
 			if(err) throw err;
 			res.send(stringifyFile);
@@ -23,6 +30,9 @@ app.post('/updateNote/:note', function(req, res) {
 		});
 	});
 });
+
+
+
 //odp 404 od serwera
 app.use(function(req, res, next) {
 	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
